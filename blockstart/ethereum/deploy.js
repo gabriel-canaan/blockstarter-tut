6e4aae0178628c680676e47de2ae11b4c9cdce9a
gabriel <777canaan@gmail.com>
2018-02-20 16:24:53 +1300

    deploy factory add next react

---
+++ blockstart/ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json')
+
+const provider = new HDWalletProvider(
+  'floor joy identify poverty aisle enough custom turkey vintage medal come under',
+  'https://rinkeby.infura.io/14oe6jqRVkJfwU14fRyl'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
--- blockstart/test/Campaign.test.js
+++ blockstart/test/Campaign.test.js
@@ -49,12 +49,27 @@ describe('Campaigns', () => {
   });
 
   it('allows a manager to make a payment request', async () => {
-    await campaign.methods
-    .createRequest('buy food', '100', accounts[2])
-    .send({
-      from : accounts[0],
-      gas : '1000000'});
+    await campaign.methods.createRequest('buy food', '100', accounts[2]).send({from: accounts[0], gas: '1000000'});
     const request = await campaign.methods.requests(0).call();
     assert.equal('buy food', request.description);
+  });
+
+  it('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    });
+
+    await campaign.methods.createRequest('eh', web3.utils.toWei('5', 'ether'), accounts[1]).send({from: accounts[0], gas: '1000000'});
+
+    await campaign.methods.approveRequest(0).send({from: accounts[0], gas: '1000000'});
+
+    await campaign.methods.finalizeRequest(0).send({from: accounts[0], gas: '1000000'});
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+    console.log(balance);
+    assert(balance > 104);
   });
 });
