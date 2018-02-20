254c9646b27848867b3f272ab484f6d22feea988
gabriel <777canaan@gmail.com>
2018-02-20 13:35:14 +1300

    add more tests

--- blockstart/test/Campaign.test.js
+++ blockstart/test/Campaign.test.js
@@ -14,20 +14,12 @@ let campaign;
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
-  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-  .deploy({data: compiledFactory.bytecode})
-  .send({from: accounts[0], gas: '1000000'});
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface)).deploy({data: compiledFactory.bytecode}).send({from: accounts[0], gas: '1000000'});
 
-  await factory.methods.createCampaign('100').send({
-    from: accounts[0],
-    gas: '1000000'
-  });
+  await factory.methods.createCampaign('100').send({from: accounts[0], gas: '1000000'});
 
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
-  campaign = await new web3.eth.Contract(
-    JSON.parse(compiledCampaign.interface),
-    campaignAddress
-  );
+  campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress);
 });
 
 describe('Campaigns', () => {
@@ -41,12 +33,28 @@ describe('Campaigns', () => {
     assert.equal(accounts[0], manager);
   });
 
-  it('allow people to contibute and marks them as approvers', async () => {
-    await campaign.methods.contribute().send({
-      value: '200',
-      from: accounts[1]
-    });
+  it('allows people to contibute and marks them as approvers', async () => {
+    await campaign.methods.contribute().send({value: '200', from: accounts[1]});
     const isContributer = await campaign.methods.approvers(accounts[1]).call();
     assert(isContributer);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({value: '50', from: accounts[1]});
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+    .createRequest('buy food', '100', accounts[2])
+    .send({
+      from : accounts[0],
+      gas : '1000000'});
+    const request = await campaign.methods.requests(0).call();
+    assert.equal('buy food', request.description);
   });
 });
